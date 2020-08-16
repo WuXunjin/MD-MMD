372b61f652005afee2e85387f1300279de75f877
sun000 <sun000mail@gmail.com>
2020-08-16 14:16:35 +0800

    add PACS datasets test dataloader

--- data_loader.py
+++ data_loader.py
@@ -58,7 +58,7 @@ def digits_loader(cfg, val=True):
 
 def PACS_loader(cfg, val=True):
     img_size = 299 if cfg.network == "inception" else 224
-    src_transform = transforms.Compose([
+    train_transform = transforms.Compose([
         transforms.Resize([256,256]),
         transforms.RandomCrop(224),
         #transforms.RandomResizedCrop(img_size),
@@ -70,8 +70,9 @@ def PACS_loader(cfg, val=True):
             std =[0.3042, 0.3135, 0.3423 ]
             )
         ])
-    tar_transform = transforms.Compose([
-        transforms.Resize(img_size),
+
+    test_transform = transforms.Compose([
+        transforms.Resize([img_size, img_size]),
         transforms.ToTensor(),
         transforms.Normalize(
             mean=[0.7609, 0.7414, 0.7128],
@@ -79,27 +80,31 @@ def PACS_loader(cfg, val=True):
             )
         ])
 
-    datasets_src, datasets_tar = [], []
+    datasets_src, datasets_tar, datasets_tar_test = [], [], []
     for src in cfg.src:
-        datasets_src.append(datasets.ImageFolder(root=cfg.data_root + src, transform=src_transform))
+        datasets_src.append(datasets.ImageFolder(root=cfg.data_root + src, transform=train_transform))
 
     for tar in cfg.tar:
-        datasets_tar.append(datasets.ImageFolder(root=cfg.data_root + tar, transform=tar_transform))
+        datasets_tar.append(datasets.ImageFolder(root=cfg.data_root + tar, transform=train_transform))
+        datasets_tar_test.append(datasets.ImageFolder(root=cfg.data_root + tar, transform=test_transform))
 
     data_src = ConcatDataset(datasets_src)
     data_tar = ConcatDataset(datasets_tar)
+    data_tar_test = ConcatDataset(datasets_tar_test)
 
     tar_loader = DataLoader(data_tar, batch_size=cfg.batch_size, shuffle=True, drop_last=True, num_workers=4, pin_memory=True)
+    tar_test_loader = DataLoader(data_tar, batch_size=cfg.batch_size, shuffle=True, drop_last=False, num_workers=4, pin_memory=True)
     if val:
         train_size = int(cfg.src_val_ratio*len(data_src))
         val_size   = len(data_src) - train_size
         data_train, data_val = random_split(data_src, [train_size, val_size])
         train_loader = DataLoader(data_train, batch_size=cfg.batch_size, shuffle=True, drop_last=True, num_workers=4, pin_memory=True)
         val_loader   = DataLoader(data_val, batch_size=cfg.batch_size, shuffle=True, drop_last=True, num_workers=4, pin_memory=True)
-        return train_loader, val_loader, tar_loader
+        return train_loader, val_loader, tar_loader, tar_test_loader
     else:
         train_loader = DataLoader(data_src, batch_size=cfg.batch_size, shuffle=True, drop_last=True, num_workers=4, pin_memory=True)
-        return train_loader, tar_loader
+        return train_loader, tar_loader, tar_test_loader
+
 
 
 
